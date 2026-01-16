2a1e31d6d5c152f3578f6fe36b0a0f27f6e00b2f
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2026-01-17 00:21:06 +0500

    Update Challenge_6__ Suspicious_Device_Detection.py

--- Challenge_6__ Suspicious_Device_Detection.py
+++ Challenge_6__ Suspicious_Device_Detection.py
@@ -1,9 +1,15 @@
-def repeatedNTimes(nums: list):
+def repeatedNTimes(nums : str):
+    nums = nums.split(' ')
+    Nums = [int(i) for i in nums]
     seen = set()
-    for i in nums:
+    for i in Nums:
         if i in seen:
             return i
         else:
             seen.add(i)
 
-
+# # Done!
+# n = '2 1 2 5 3 2'
+# output =repeatedNTimes(n)
+# print("Input:",n)
+# print("Output:",output)
