14eb496b7db6b5ec41dc03881c3f9adc6e275f56
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2026-01-16 21:40:23 +0500

    Update Challenge_5__Longest_Mirrored_Phrase.py

--- Challenge_5__Longest_Mirrored_Phrase.py
+++ Challenge_5__Longest_Mirrored_Phrase.py
@@ -1,13 +1,16 @@
 def longest_palindromic_substring(s):
+    max = ''
     for i in range(len(s)):
         if len(s) % 2 == 0:
             L = i
             R = i + 1
         else:
             L = R = i
-        max = ''
+
         while L>=0 and R < len(s) and s[L] == s[R]:
-            max = s[L: R+1]
+            if len(s[L: R+1]) >= len(max):
+                max = s[L: R+1]
+                print('max:',i,max)
             L -= 1
             R += 1
 
