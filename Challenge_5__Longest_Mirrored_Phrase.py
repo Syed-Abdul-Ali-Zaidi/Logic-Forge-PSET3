24d4ac390d653821b9b1d603b8862a47d1cffd43
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2026-01-16 21:54:19 +0500

    Update Challenge_5__Longest_Mirrored_Phrase.py

--- Challenge_5__Longest_Mirrored_Phrase.py
+++ Challenge_5__Longest_Mirrored_Phrase.py
@@ -1,17 +1,33 @@
 def longest_palindromic_substring(s):
-    max = ''
+    Max = ''
     for i in range(len(s)):
-        if len(s) % 2 == 0:
-            L = i
-            R = i + 1
-        else:
-            L = R = i
-
+        # Checking while Centre is single character
+        L = R = i
         while L>=0 and R < len(s) and s[L] == s[R]:
-            if len(s[L: R+1]) >= len(max):
-                max = s[L: R+1]
-                print('max:',i,max)
+            if len(s[L: R+1]) >= len(Max):
+                Max = s[L: R+1]
             L -= 1
             R += 1
 
-    return max
+        # Checking while Centre is two characters
+        L = i
+        R = i + 1
+        while L>=0 and R < len(s) and s[L] == s[R]:
+            if len(s[L: R+1]) >= len(Max):
+                Max = s[L: R+1]
+            L -= 1
+            R += 1
+
+    return Max
+
+# Done!
+# string = 'babad'
+# output = longest_palindromic_substring(string)
+# print("Input:",string)
+# print("Output:",output)
+#
+# string = 'bab'
+# output = longest_palindromic_substring(string)
+# print("Input:",string)
+# print("Output:",output)
+
