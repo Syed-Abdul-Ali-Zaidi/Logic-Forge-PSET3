a173386a6dd5b160185ed590b6140f91a6650c3f
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2026-01-18 16:45:49 +0500

    Update Challenge_1__Smart_City_Temp.py

--- Challenge_1__Smart_City_Temp.py
+++ Challenge_1__Smart_City_Temp.py
@@ -1,40 +1,42 @@
-first = input()     # Taking first line input
-first = first.split(' ')
-N = int(first[0])
-K = int(first[1])
-Q = int(first[2])
+def smart_temp():
+    first = input()     # Taking first line input
+    first = first.split(' ')
+    N = int(first[0])
+    K = int(first[1])
+    Q = int(first[2])
 
-temp = input()     # Taking Temperatures input
-temp = temp.split(' ')
-temp = [int(i) for i in temp]
+    temp = input()     # Taking Temperatures input
+    temp = temp.split(' ')
+    temp = [int(i) for i in temp]
 
-queries = []
-# Taking queries input
-for i in range(Q):
-    query = input()
-    query = query.split(' ')
-    queries.append(query)
+    queries = []
+    # Taking queries input
+    for i in range(Q):
+        query = input()
+        query = query.split(' ')
+        queries.append(query)
 
-# Processing Alert day for each day
-alert = ['No Alert' for i in range(N)]
-for i in range(N-1):
-    for j in range(i+1,N):
-        if (temp[j] >= temp[i] + K) or (temp[j] <= temp[i] - K):
-            alert[i] = j
-            break
+    # Processing Alert day for each day
+    alert = ['No Alert' for i in range(N)]
+    for i in range(N-1):
+        for j in range(i+1,N):
+            if (temp[j] >= temp[i] + K) or (temp[j] <= temp[i] - K):
+                alert[i] = j
+                break
 
-# Processing queries
-for i in queries:
-    if i[0] == 'NEXT':
-        alert_day = alert[int(i[1])]
-        print(alert_day)
-    else:
-        L = int(i[1])
-        R = int(i[2])
-        count = 0
-        for i in range(L,R+1):
-            if str(alert[i]).isdigit():
-                count += 1
-        print(count)
+    # Processing queries
+    for i in queries:
+        if i[0] == 'NEXT':
+            alert_day = alert[int(i[1])]
+            print(alert_day)
+        else:
+            L = int(i[1])
+            R = int(i[2])
+            count = 0
+            for i in range(L,R+1):
+                if str(alert[i]).isdigit():
+                    count += 1
+            print(count)
 
 # Done!
+smart_temp()
